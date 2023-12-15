7f47c88b90d2b596baf2291f86ede561dc264983
Sebastian Hirt <sebastian@hirts.de>
2023-12-15 03:09:49 +0100

    setup

---
+++ my-app/src/Content.tsx
@@ -0,0 +1,41 @@
+import { createEffect, createResource, createSignal, onMount } from "solid-js";
+import { Mathjax } from "mathjax";
+
+export function Content(props: {url: string}) {
+
+    const [url, setUrl] = createSignal(props.url);
+    const [content] = createResource(url, fetchContent);
+
+
+    onMount(() => {
+        console.log(`Content mounted`);
+        if (window.MathJax) {
+            console.log(`MathJax already loaded`);
+            refreshMath();
+        } else {
+            let mathJaxScript = document.querySelector('#MathJax-script');
+            mathJaxScript?.addEventListener('load', refreshMath);
+        }
+    });
+
+    return (
+    <div>
+    <p>Content:</p>
+    <div>{props.url}</div>
+    <div innerHTML={content()}></div>
+    </div>
+    )
+}
+
+const fetchContent = async (name: string) => {
+    let url = `/data/${name}.html`;
+    console.log(`fetching ${url}`);
+    const response = await fetch(url);
+    const text = await response.text();
+    return text;
+}
+
+function refreshMath() {
+    window.MathJax.typeset();
+    console.log(`Math refreshed`);
+}
---
+++ my-app/src/Search.tsx
@@ -0,0 +1,10 @@
+import { createSignal } from "solid-js";
+
+export function Search() {
+    return (
+    <div>
+    <p>Search:</p>
+    <input type="text" placeholder="Search" />
+    </div>
+    )
+}
